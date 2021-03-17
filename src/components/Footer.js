02fe1bc09a19f82d97f04eed1b3441e79976b3da
Ryan Montgomery <44453666+0dayCTF@users.noreply.github.com>
2021-03-17 19:34:52 -0400

    Update Footer.js
    Added link to revshells.com

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer>
-      <h2>Concept and formatting with inspiration from <a ref="https://revshells.com">revshells.com ❤</a></h2>
+      <h2>Concept and formatting with inspiration from <a href="https://revshells.com">revshells.com ❤</a></h2>
     </footer>
   );
 };
